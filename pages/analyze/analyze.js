ecfa72585abe12f62917f9a1c2d6a993b5afd65f
menyanyu <781689161@qq.com>
2022-10-08 23:39:17 +0800

    完善分析

--- pages/analyze/analyze.js
+++ pages/analyze/analyze.js
@@ -7,19 +7,69 @@ Page({
         redBallList: [0, 0, 0, 0, 0, 0],
         blueBall: 0,
         delay: "500",
-        btnDis: false,
+        btnDis1: false,
+        btnDis2: true,
         redDuration: 1500,
-        blueDuration: 1500
+        blueDuration: 1500,
+        display: false,
+        analysisResults: null
     },
 
+
+    //开始滚动
     startRoll() {
         const p = new Promise((resolve, reject) => {
             this.randomNum()
+            let redBall = this.data.redBallList
+            const strArr = redBall.map(item => {
+                let str = item.toString()
+                if (str.length == 1) { str = "0" + str }
+                return str
+            })
+            let blue = this.data.blueBall.toString()
+            if (blue.length == 1) { blue = "0" + blue }
+            const str = strArr.toString() + "+" + blue
+            wx.request({
+                method: "GET",
+                url: 'http://localhost:3000/users/lottery/red/count',
+                data: { data: str },
+                timeout: 10000,
+                success: res => {
+                    let json = []
+                    for (let i = 0; i < 6; i++) {
+                        let redAttribute = res.data.data.redForget[i]
+                        if (redAttribute>6) {
+                            redAttribute="冷号"
+                        }else{
+                            redAttribute="热号"
+                        }
+
+
+                        json.push({
+                            redAppearCount:res.data.data.redAppearCount[i],
+                            redForget:res.data.data.redForget[i],
+                            redProbabilit:res.data.data.redProbability[i],
+                            redBall:this.data.redBallList[i],
+                            redAttribute
+                        })
+                    }
+
+                    this.setData({
+                        analysisResults: json
+                    })
+                    console.log(json)
+                },
+                fail: err => {
+                    console.log(err)
+                }
+
+            })
+
             for (let i = 0; i < 6; i++) {
                 var t1 = setTimeout(() => {
                     this.selectComponent(`#reds${[i]}`).start();
                     this.setData({
-                        btnDis: true
+                        btnDis1: true
                     })
                 }, i * this.data.delay);
             }
@@ -31,7 +81,8 @@ Page({
         }).then(() => {
             const t3 = setTimeout(() => {
                 this.setData({
-                    btnDis: false
+                    btnDis1: false,
+                    btnDis2: false
                 })
             }, this.data.blueDuration);
         })
@@ -55,8 +106,24 @@ Page({
             redBallList: list,
             blueBall: blue
         })
-
     },
+
+    //开始分析
+    startAnalysis() {
+        wx.showLoading({
+            title: '分析中',
+        })
+        setTimeout(() => {
+            wx.hideLoading({
+                success: (res) => {
+                    this.setData({
+                        display: true
+                    })
+                },
+            })
+        }, 1000);
+    },
+
 
     /**
      * 生命周期函数--监听页面加载
